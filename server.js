709caa405dba5bf27182d0a40da47de5618f19eb
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-07-04 11:20:19 -0400

    add minimal code for simple GET route

--- server.js
+++ server.js
@@ -4,8 +4,19 @@ const fs = require('fs');
 const path = require('path');
 
 // require data
-const { notes } = require('./Develop/db/db.json');
+const [ notes ] = require('./Develop/db/db.json');
 
 
 // instantiate the server
 const app = express();
+
+
+// GET route
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
