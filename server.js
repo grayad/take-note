64a2e7ea766a5a7b79a1451242a07d57c712d55e
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-07-05 16:25:51 -0400

    add minimal code to POST new note

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ const path = require('path');
 const [ notes ] = require('./Develop/db/db.json');
 
 // use PORT if it has been set, or default to 3001
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // instantiate the server
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 // return ID of specific note
 // function findById(id, notesArray) {
@@ -18,6 +23,18 @@ const app = express();
 //     return result;
 // }
 
+function createNewNote(body, notes) {
+    const note = body;
+    // save new data in local server.js copy of animal data
+    [notes].push(note);
+
+    // write new data to db.json
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/db.json'),
+        // convert JS array data to JSON, do not change current data, and leave white space for readability
+        JSON.stringify([notes], null, 2)
+    );
+}
 // GET route
 app.get('/api/notes', (req, res) => {
     res.json(notes);
@@ -29,6 +46,11 @@ app.get('/api/notes', (req, res) => {
 //       res.json(result);
 // });
 
+// POST new notes
+app.post('/api/notes', (req,res) => {
+    const note = createNewNote(req.body, notes);
+    res.json(req.body);
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
